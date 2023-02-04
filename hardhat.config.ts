e9c72fe89d8ce4139d1c2e1a64ebbef3afb8f3ed
SethLawson07 <latesethlawsonhetchely@gmail.com>
2023-02-04 12:35:42 +0000

    Implementation and Deployment of the GoldenMan contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      // This value will be replaced on runtime
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      url: process.env.PROD_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
